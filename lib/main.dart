160dc651f082f7ad42b60d053f2d8aca15a4b113
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-06-01 18:04:04 -0300

    Change primary color

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const int _primarycolorPrimaryValue = 0xFF00384C;
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MaxFlix',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: const MaterialColor(_primarycolorPrimaryValue, <int, Color>{
+          50: Color(0xFFE0E7EA),
+          100: Color(0xFFB3C3C9),
+          200: Color(0xFF809CA6),
+          300: Color(0xFF4D7482),
+          400: Color(0xFF265667),
+          500: Color(_primarycolorPrimaryValue),
+          600: Color(0xFF003245),
+          700: Color(0xFF002B3C),
+          800: Color(0xFF002433),
+          900: Color(0xFF001724),
+        }),
       ),
       debugShowCheckedModeBanner: false,
       home: const Home(),
